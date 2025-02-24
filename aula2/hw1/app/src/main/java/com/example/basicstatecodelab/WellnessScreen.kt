e48e083205a69e678edf80a9f5a1096a6b67731a
Abel Teixeira <120480217+ttabelhaxd@users.noreply.github.com>
2025-02-24 18:12:27 +0000

    hw1

---
+++ aula2/hw1/app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,26 @@
+package com.example.basicstatecodelab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task ->
+                wellnessViewModel.remove(task)
+            }
+        )
+    }
+}
