374d5e83cd323c0c1c84325ea940d93ea7580db7
Abel Teixeira <120480217+ttabelhaxd@users.noreply.github.com>
2025-03-22 18:35:15 +0000

    aula6

---
+++ aula6/codelab-gemini-summary-main/app/src/main/java/com/example/jetnews/data/gemini/impl/GeminiRepositoryImpl.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.jetnews.data.gemini.impl
+
+import com.example.jetnews.data.gemini.GeminiRepository
+import com.example.jetnews.model.Post
+import com.google.firebase.Firebase
+import com.google.firebase.vertexai.type.HarmBlockThreshold
+import com.google.firebase.vertexai.type.HarmCategory
+import com.google.firebase.vertexai.type.SafetySetting
+import com.google.firebase.vertexai.type.generationConfig
+import com.google.firebase.vertexai.vertexAI
+
+class GeminiRepositoryImpl: GeminiRepository {
+
+
+    private val generativeModel = Firebase
+        .vertexAI
+        .generativeModel(
+            modelName = "gemini-1.5-flash",
+            generationConfig = generationConfig {
+                temperature = 0f
+            },
+            safetySettings = listOf(
+                SafetySetting(HarmCategory.HARASSMENT, HarmBlockThreshold.LOW_AND_ABOVE),
+                SafetySetting(HarmCategory.HATE_SPEECH, HarmBlockThreshold.LOW_AND_ABOVE),
+                SafetySetting(HarmCategory.SEXUALLY_EXPLICIT, HarmBlockThreshold.LOW_AND_ABOVE),
+                SafetySetting(HarmCategory.DANGEROUS_CONTENT, HarmBlockThreshold.LOW_AND_ABOVE),
+            )
+        )
+
+    override suspend fun summarizePost(post: Post): String? {
+        val stringBuilder = StringBuilder()
+
+        for (paragraph in post.paragraphs) {
+            stringBuilder.append(paragraph.text)
+        }
+
+        val prompt =
+            "Summarize the following article in 4 concise bullet points. " +
+                    "Ensure each bullet point is specific, informative and relevant. " +
+                    "Return just the bullet points as plain text. " +
+                    "Use plain text, don't use markdown. \n $stringBuilder"
+
+        return generativeModel.generateContent(prompt).text
+    }
+
+}
